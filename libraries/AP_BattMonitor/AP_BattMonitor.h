cabe03c39195a2dde1815cf81a2f6881666af4ff
Craig R. Hughes <craig.git@rungie.com>
2014-06-12 14:08:41 -0700

    [CRHJet] Adjust ratio based on measured values rather than based on theoretical resistances

--- libraries/AP_BattMonitor/AP_BattMonitor.h
+++ libraries/AP_BattMonitor/AP_BattMonitor.h
@@ -54,7 +54,7 @@
 #elif CONFIG_HAL_BOARD == HAL_BOARD_MPNG
  # define AP_BATT_VOLT_PIN                  1
  # define AP_BATT_CURR_PIN                  -1
- # define AP_BATT_VOLTDIVIDER_DEFAULT       ((100.0+47.0)/47.0)
+ # define AP_BATT_VOLTDIVIDER_DEFAULT       (9.75/2.53)
  # define AP_BATT_CURR_AMP_PERVOLT_DEFAULT  17.0
 #else
  # define AP_BATT_VOLT_PIN                  -1
